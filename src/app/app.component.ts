4eca756fbc5a59d51e273c7d37eeecfe0572ecd5
ravikumar444411 <ravikumar444411@gmail.com>
2022-04-14 22:47:49 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 })
 export class AppComponent  {
   title = 'solarCell';
-
+ 
 
 
   CURRENT_DENSITY:number = 32.7
@@ -17,10 +17,13 @@ export class AppComponent  {
   JSC:number = 0;
   minJSC:number = 18;
   maxJSC:number = 21;
+  show_ff:number = 0;
+  powernInput:any = 10
+  show_efficency:number = 0
 
 
 
-  cur_area:number = 0; // for area
+  cur_area:number = 100; // for area
   currentDensitySC:any = [];  //for Jsc
   
   //calculate for Isc
@@ -34,8 +37,17 @@ export class AppComponent  {
   voltageTable:any = []
   cur_n:number = 1;
   cur_k:number = 1.38064852 * Math.pow(10, -23);
-  cur_Temp:any = 274
+  cur_Temp:any = 298
   cur_q:number = 1.602176634 * Math.pow(10,-19);
+
+
+  // for Isc vs Voc
+  bothIscVoc:any;
+  //calculate for ff
+  ffData:any = []
+
+
+
 
 
   // g_a = gallium arsenide
@@ -47,12 +59,13 @@ export class AppComponent  {
 options: any;
 
     constructor() {
- 
+        
+        // for Isc
         this.dataISC = {
             labels: [],
             datasets: [
                 {
-                    label: 'Short circuit current ',
+                    label: 'Isc vs Jsc',
                     data: [],
                     fill: false,
                     borderColor: '#42A5F5',
@@ -68,7 +81,7 @@ options: any;
             labels: [],
             datasets: [
                 {
-                    label: 'Open Circuit Voltage',
+                    label: 'Voc vs Jsc',
                     data: [],
                     fill: false,
                     borderColor: '#FFA726',
@@ -76,6 +89,21 @@ options: any;
                 }
             ]
         };
+
+        // for Isc vs Voc
+         this.bothIscVoc = {
+            labels: [],
+            datasets: [
+                {
+                    label: 'Isc vs Voc',
+                    data: [],
+                    fill: false,
+                    borderColor: '#FFA726',
+                    tension: .4
+                }
+            ]
+        };
+
 
         this.options = {
             plugins: {
@@ -116,7 +144,9 @@ selectMaterial(material_name:string){
 
     }
     else if(material_name==='a_s'){ 
-        this.CURRENT_DENSITY = 15.32
+        this.CURRENT_DENSITY = 9.62
+        this.minJSC = 14
+        this.maxJSC = 26
     }
     else if(material_name==='c_t'){
         this.CURRENT_DENSITY = 28
@@ -147,24 +177,39 @@ setArea(area_value:any){
    
 }
 
-  addToTable(){
+addToTable(){
 
 
       //insert data in jsc table
-      this.currentDensitySC.push(this.JSC)
+      this.currentDensitySC.push( this.makeRoundOff(this.JSC * Math.pow(10,-3) ) )
       
       // to calculage Isc
-      var I_SC = this.JSC * this.cur_area;
+      var I_SC = this.makeRoundOff( this.JSC * Math.pow(10,-3) * this.cur_area );
       const rowISC = { JSC:this.JSC,I_SC:I_SC}
       this.currentSC.push(I_SC) // to store I_sc
       this.currentTable.push(rowISC) // to store Isc table
 
 
     // to calculate Voc
-   var V_OC =  this.cur_n * this.cur_k * this.cur_Temp * Math.log( (this.JSC * this.CURRENT_DENSITY) + 1 ) / this.cur_q
-   this.voltageSC.push(V_OC)
-   const rowVOC = { JSC:this.JSC,V_OC:V_OC}
+    var totalConst = 100 * this.cur_n * this.cur_k * this.cur_Temp / this.cur_q 
+    var logval = Math.log( (this.JSC / this.CURRENT_DENSITY) + 1 )
+   var V_OC =  (100 * this.cur_n * this.cur_k * this.cur_Temp / this.cur_q ) * Math.log( (this.JSC / this.CURRENT_DENSITY) + 1 ) 
+   this.voltageSC.push( this.makeRoundOff( V_OC) )
+   const rowVOC = { JSC:this.JSC,V_OC: this.makeRoundOff( V_OC) }
    this.voltageTable.push(rowVOC)
+
+
+
+   console.log('const', totalConst);
+   console.log('logval', logval);
+   console.log('voc', V_OC);
+
+   console.log('n',this.cur_n);
+   console.log('k',this.cur_k);
+   console.log('T',this.cur_Temp);
+   console.log('q',this.cur_q);
+   console.log('j',this.JSC);
+   console.log('j0',this.CURRENT_DENSITY);
 
     
   }
@@ -177,51 +222,57 @@ resetGraph(){
     this.currentTable = []
     this.voltageTable = []
 }
-  plotGraph = (event: Event)=> {
-      console.log(this.currentDensitySC);
-      console.log(this.voltageSC);
-      
-    // this.data = {
-    //     labels: this.areaData,
-    //     datasets: [
-    //         {
-    //             label: 'Short circuit current ',
-    //             data: this.currentData,
-    //             fill: false,
-    //             borderColor: '#42A5F5',
-    //             tension: .4
-    //         },
-    //         {
-    //             label: 'Open Circuit Voltage',
-    //             data: [28, 48, 40, 19, 86, 27, 90],
-    //             fill: false,
-    //             borderColor: '#FFA726',
-    //             tension: .4
-    //         }
-    //     ]
-    // };
-      
-    // this.options = {
-    //     title: {
-    //         display: true,
-    //         text: 'My Title',
-    //         fontSize: 16
-    //     },
-    //     legend: {
-    //         position: 'bottom'
-    //     }
-    // };
-}
-
-
 
 // plot graph for voc
 plotGraphVOC = (event:Event) =>{
+    // add Isc zero to array
+    this.currentDensitySC.push(0)
+    this.currentSC.push(0)
+    this.voltageSC.push(0)
+
+    // console.log(this.ffData);
+    // const sum = this.ffData.reduce((a:any, b:any) => a + b, 0);
+    // const avg = (sum / this.ffData.length) || 0;
+    // this.show_ff = (Math.round(avg * 100) / 100).toFixed(2);  // put avg ff in here
+
+    // find max value from I (current)  array to get Isc
+    const max_Isc = this.currentSC.reduce(function(prev:any, current:any) {
+        return (prev > current) ? prev : current
+    }) 
+    // find max value from V (voltage)  array to get Voc
+    const max_Voc = this.voltageSC.reduce(function(prev:any, current:any) {
+        return (prev > current) ? prev : current
+    }) 
+
+    var mx_voc:number =  +max_Voc;
+
+
+    
+    var fillFactor = (mx_voc - Math.log(mx_voc + 0.72) )/ (mx_voc + 1)
+    this.show_efficency = max_Isc * max_Voc * this.show_ff * 100 / this.powernInput
+
+    // console.log('voc', mx_voc);
+    // console.log('fill factor', fillFactor);
+    // console.log('%', this.show_efficency);
+
+
+    // console.log('n',this.cur_n);
+    // console.log('k',this.cur_k);
+    // console.log('T',this.cur_Temp);
+    // console.log('q',this.cur_q);
+    // console.log('j',this.JSC);
+    // console.log('j0',this.CURRENT_DENSITY);
+    
+    
+
+    // console.log(`The sum is: ${sum}. The average is: ${avg}.`);
+
+    
     this.dataISC = {
         labels: this.currentDensitySC,
         datasets: [
             {
-                label: 'Short circuit current ',
+                label: 'Isc vs Voc ',
                 data: this.currentSC,
                 fill: false,
                 borderColor: '#42A5F5',
@@ -233,7 +284,7 @@ plotGraphVOC = (event:Event) =>{
         labels: this.currentDensitySC,
         datasets: [
             {
-                label: 'Open circuit voltage ',
+                label: 'Voc vs Jsc',
                 data: this.voltageSC,
                 fill: false,
                 borderColor: '#FFA726',
@@ -242,6 +293,19 @@ plotGraphVOC = (event:Event) =>{
         ]
     };
       
+    // for Isc vs Voc
+    this.bothIscVoc = {
+        labels: this.voltageSC,
+        datasets: [
+            {
+                label: 'Isc vs Voc',
+                data: this.currentSC,
+                fill: false,
+                borderColor: '#FFA726',
+                tension: .4
+            }
+        ]
+    };
     this.options = {
         title: {
             display: true,
@@ -257,5 +321,7 @@ plotGraphVOC = (event:Event) =>{
 
 
 
-
+makeRoundOff(value:number){
+    return (Math.round(value * 100) / 100).toFixed(2);
+}
 }
